ef89abe3e08ab9e267d5a7cf44e6016f7f097dc4
Carlos Roa <carlosroab95@gmail.com>
2023-07-25 12:15:26 -0300

    otros cambios

--- src/components/itemCart.jsx
+++ src/components/itemCart.jsx
@@ -1,39 +1,56 @@
 import { useCartContext } from "../context/CartContext";
-
+import "../App.css";
 const ItemCart = ({ product }) => {
   const { removeFromCart } = useCartContext();
   return (
-    <>
-      <div class="card mb-3">
-        <div class="row g-0">
-          <div class="col-md-6">
-            <img
-              className="card-img-top"
-              src={product.img}
-              alt="Card image cap"
-              style={{ width: 250 }}
-            />
+    <div className="product-container my-5 rounded rounded-sm">
+      <div className="d-flex space-between w-100 description-container">
+        <div className="col-2">
+          <h5>Producto</h5>
+        </div>
+
+        <div className="d-flex container-fluid justify-content-around names-container col-6">
+          <h5>Precio</h5>
+          <h5>Cantidad</h5>
+          <h5>Subtotal</h5>
+        </div>
+      </div>
+
+      <div className="product-container ">
+        <hr />
+        <div className="d-flex space-between  product-container-description">
+          <div class="col-5">
+            <div className="d-flex align-items-center shoes-text">
+              <img
+                className="card-img-top"
+                src={product.img}
+                alt="Card image cap"
+                style={{ width: 200 }}
+              />
+              <h5 className="">{product.name}</h5>
+            </div>
           </div>
-          <div class="col-md-6">
-            <div class="card-body">
-              <h5 className="card-title">{product.name}</h5>
-              <p className="card-text">Cantidad {product.quantity}</p>
-              <p className="card-text">${product.price}</p>
-              <p className="card-text">
-                Sub Total ${product.quantity * product.price}
-              </p>
+
+          <div class="d-flex justify-content-around align-items-center col-7 product-description">
+            <h5 class="responsive-text">${product.price}</h5>
+            <h5 class="responsive-text-cantidad">${product.quantity}</h5>
+            <h5 class="responsive-text-subtotal">
+              ${product.quantity * product.price}
+            </h5>
+
+            {
               <button
                 onClick={() => removeFromCart(product.id)}
                 class="btn btn-primary"
               >
                 Eliminar
               </button>
-            </div>
+            }
           </div>
         </div>
+        <hr />
       </div>
-      ;
-    </>
+    </div>
   );
 };
 
